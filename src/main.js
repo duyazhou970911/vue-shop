5a7c8c2cc7e30a86e3da983251c6516aa3f426af
duyazhou <itheima@itcast.cn>
2020-11-25 19:30:23 +0800

    杜亚洲的第一次提交

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+Vue.prototype.$http = axios
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
